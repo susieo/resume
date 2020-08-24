5ac0264cb6a729adbfa9e3da0a3bbc54525ce4f4
susieo <susytuty@gmail.com>
2020-08-24 11:02:06 -0700

    Updates

---
+++ precache-manifest.6e256dd8067e1f29d1e1956551cd8923.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff1bb7542bb5bd4973c03f92ea2ff57",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "db275203ad73b71e7703",
+    "url": "/resume/static/css/2.bce90bec.chunk.css"
+  },
+  {
+    "revision": "2940e883cf7062612d72",
+    "url": "/resume/static/css/main.78def9c8.chunk.css"
+  },
+  {
+    "revision": "db275203ad73b71e7703",
+    "url": "/resume/static/js/2.140044b4.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/resume/static/js/2.140044b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2940e883cf7062612d72",
+    "url": "/resume/static/js/main.ea6553b6.chunk.js"
+  },
+  {
+    "revision": "472359a62323bf7c860a",
+    "url": "/resume/static/js/runtime-main.6921eb3d.js"
+  },
+  {
+    "revision": "ecb5066bde079cbaebae18ff27b59e91",
+    "url": "/resume/static/media/1984-v2.ecb5066b.jpg"
+  },
+  {
+    "revision": "d613428f2410ee19cb8231b2d252a117",
+    "url": "/resume/static/media/pug.d613428f.jpg"
+  }
+]);
